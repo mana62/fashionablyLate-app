74a898caafd0ae4690b4994289776616af8a81f4
Mana Fukumoto <mana66354@gmail.com>
2024-09-12 21:46:29 +0900

    機能の追加など

--- src/resources/views/attendance-sheet.blade.php
+++ src/resources/views/attendance-sheet.blade.php
@@ -8,6 +8,10 @@
     <ul class="header-nav-list">
         <li class="header-nav-item"><a href="{{ route('stamping') }}">ホーム</a></li>
         <li class="header-nav-item"><a href="{{ route('attendance-sheet') }}">日付一覧</a></li>
+        <li class="header-nav-item"><a href="{{ route('users-index') }}">メンバー一覧</a></li>
+        @if(Auth::check())
+        <li class="header-nav-item"><a href="{{ route('users-attendance', ['user' => Auth::user()->id]) }}">メンバー毎の勤務一覧</a></li>
+        @endif
         <li class="header-nav-item">
             <a class="dropdown-item" href="{{ route('logout') }}"
                 onclick="event.preventDefault();document.getElementById('logout-form').submit();">
--- src/resources/views/stamping.blade.php
+++ src/resources/views/stamping.blade.php
@@ -8,7 +8,19 @@
     <ul class="header-nav-list">
         <li class="header-nav-item"><a href="{{ route('stamping') }}">ホーム</a></li>
         <li class="header-nav-item"><a href="{{ route('attendance-sheet') }}">日付一覧</a></li>
-        <li class="header-nav-item"><a href="{{ route('login') }}">ログアウト</a></li>
+        <li class="header-nav-item"><a href="{{ route('users-index') }}">メンバー一覧</a></li>
+        @if(Auth::check())
+        <li class="header-nav-item"><a href="{{ route('users-attendance', ['user' => Auth::user()->id]) }}">メンバー毎の勤務一覧</a></li>
+        @endif
+        <li class="header-nav-item">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                {{ __('ログアウト') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
+                @csrf
+            </form>
+        </li>
     </ul>
 @endsection
 
@@ -32,8 +44,8 @@
                     @csrf
                     <input type="hidden" name="start_time" value="1">
                     <div class="attendance-button">
-                        <button id="startButton" class="attendance-button__submit" type="submit" {{-- 勤務開始ボタンの有効化条件：まだ勤務を開始していないか、新しい日なら有効 --}}
-                        {{ $attendance && \Carbon\Carbon::parse($attendance->start_time)->isToday() && !$attendance->finish_time ? 'disabled' : '' }}>
+                        <button id="startButton" class="attendance-button__submit" type="submit"
+                            {{ $attendance && \Carbon\Carbon::parse($attendance->start_time)->isToday() && !$attendance->finish_time ? 'disabled' : '' }}>
                             {{-- attendanceがあるか確認、かつ今日の出勤タイムがあるか確認、かつ出勤がまだ終了していない場合無効化 --}}
                             勤務開始
                         </button>
@@ -46,7 +58,7 @@
                     <input type="hidden" name="start_break" value="1">
                     <div class="attendance-button">
                         <button id="startBreakButton" class="attendance-button__submit" type="submit" {{-- 勤務開始がされており、休憩中でない場合に有効 --}}
-                        {{ !$attendance || !$attendance->start_time || $attendance->finish_time || $attendance->breakTimes->whereNull('finish_break')->count() > 0 ? 'disabled' : '' }}>
+                            {{ !$attendance || !$attendance->start_time || $attendance->finish_time || $attendance->breakTimes->whereNull('finish_break')->count() > 0 ? 'disabled' : '' }}>
                             {{-- attendanceがない、または出勤が開始していない、または勤務終了している、または、まだ終了していない休憩が存在する場合は無効化 --}}
                             休憩開始
                         </button>
@@ -61,7 +73,7 @@
                     <input type="hidden" name="finish_time" value="1">
                     <div class="attendance-button">
                         <button id="finishButton" class="attendance-button__submit" type="submit" {{-- 勤務が開始されており、まだ終了していない場合に有効 --}}
-                        {{ !$attendance || !$attendance->start_time || $attendance->finish_time ? 'disabled' : '' }}>
+                            {{ !$attendance || !$attendance->start_time || $attendance->finish_time ? 'disabled' : '' }}>
                             勤務終了
                         </button>
                     </div>
@@ -84,8 +96,11 @@
     </div>
 
 
-<script>
-    // サーバーからのデータを使ってボタンの状態をJavaScriptでも操作する
-    window.attendanceData = @json($attendance);
-</script>
+    <!-- サーバーからのデータをJavaScriptに渡す -->
+    <script>
+        window.attendanceData = @json($attendance);
+    </script>
+
+    <!-- JavaScriptファイルを読み込む -->
+    <script src="{{ asset('js/stamping.js') }}"></script>
 @endsection
